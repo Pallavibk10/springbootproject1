59ac0887f0f111bd8a4540e34b468ffd1aa4f906
Pallavibk10 <56583978+Pallavibk10@users.noreply.github.com>
2024-02-01 13:10:30 +0530

    Add files via upload

---
+++ src/test/java/com/pallavi/springbootproject1/Springbootproject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pallavi.springbootproject1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springbootproject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
